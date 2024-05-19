4b55aea09f9c587abb617d9a8eab3bae51b01823
limingcan-home <804666207@qq.com>
2024-05-19 19:33:20 +0800

    修改框架

---
+++ rollup.config.js
@@ -0,0 +1,162 @@
+import babel from '@rollup/plugin-babel';
+import commonjs from '@rollup/plugin-commonjs';
+import resolve from '@rollup/plugin-node-resolve';
+import filesize from 'rollup-plugin-filesize';
+import {terser} from 'rollup-plugin-terser';
+import postcss from 'rollup-plugin-postcss';
+import pkg from './package.json';
+
+// let 
+// IS_PRO = process.env.NODE_ENV.includes('pro') ? true : false,
+// pluginsConfig = [
+//     // 处理css
+//     postcss({
+//         minimize: true
+//     }),
+
+//     // 显示文件大小
+//     filesize(),
+//     // 编译es6
+//     babel({
+//         // babelHelpers: 'runtime',
+//         exclude: 'node_modules/**'
+//     }),
+//     commonjs(),
+//     resolve()
+// ],
+// compressPlugin = terser({
+//     format: {
+//         comments: RegExp(`${pkg.name}`)
+//     }
+// }),
+// finalConfig = [];
+
+// IS_PRO && pluginsConfig.push(compressPlugin);
+
+// // 开始配置
+// Base.config.forEach(item => {
+//     const _config = {
+//         input: item.input,
+//         output: !IS_PRO ? item.devOutput : item.proOutput,
+//         plugins: pluginsConfig
+//     };
+//     finalConfig.push(_config);
+
+//     // console.log(!IS_PRO ? item.devOutput : item.proOutput);
+// });
+
+// // 输出配置
+// export default finalConfig;
+
+const bannerData =
+`/**!
+* ${pkg.name}: - v${pkg.version}
+* @author: ${pkg.author}
+* @date: ${new Date().getFullYear()}.${parseInt(new Date().getMonth()+1)}.${new Date().getDate()}
+* @issues: https://github.com/limingcan562/fecmjs/issues
+*/`;
+
+const defaultOutBase = {compact: true, banner: bannerData, name: pkg.name, plugins: [filesize()]};
+const cjOutBase = {...defaultOutBase, compact: false, format: "cjs", exports: "named"};
+const esmOutBase = {...defaultOutBase, format: "es"};
+const umdOutBase = {...defaultOutBase, format: "umd"};
+const minOutBase = {...defaultOutBase, plugins: [ terser() ], sourcemap: true};
+
+export default [
+    {
+        input: "./src/index.js",
+        output: [
+            {
+                ...cjOutBase,
+                file: `dist/${pkg.name}.cjs.js`,
+            },
+            {
+                ...esmOutBase,
+                file: `dist/${pkg.name}.esm.js`
+            },
+            {
+                ...umdOutBase,
+                file: `dist/${pkg.name}.js`
+            },
+            {
+                ...umdOutBase,
+                ...minOutBase,
+                file: `dist/${pkg.name}.min.js`
+            },
+        ],
+        plugins: [
+            babel({
+                exclude: "node_modules/**",
+                babelHelpers: 'bundled',
+            }),
+            resolve(), 
+            commonjs()
+        ]
+        // plugins: [
+        //     commonjs(),
+        //     resolve(),
+        //     filesize(),
+        //     babel({
+        //         exclude: "node_modules/**",
+        //         babelHelpers: 'bundled',
+        //     }),
+        // ],
+    },
+    
+    // {
+    //     input: "./src/index.js",
+    //     output: {
+    //         file: `build/${pkg.name}.esm.js`, // 导出文件
+    //         format: 'esm',
+    //         name: "fecmj",
+    //         exports: 'named',
+    //     },
+    //     plugins: [
+    //         commonjs(),
+    //         resolve(),
+    //         filesize(),
+    //         babel({
+    //             exclude: "node_modules/**",
+    //             babelHelpers: 'bundled',
+    //         }),
+    //     ],
+    // },
+
+    // {
+    //     input: "./src/index.js",
+    //     output: {
+    //         file: `dist/${pkg.name}.js`, // 导出文件
+    //         format: "umd", // 打包文件支持的形式
+    //         name: pkg.name,
+    //         exports: 'named',
+    //     },
+    //     plugins: [
+    //         commonjs(),
+    //         resolve(),
+    //         babel({
+    //             exclude: "node_modules/**",
+    //             babelHelpers: 'bundled',
+    //         }),
+    //     ],
+    // },
+    // {
+    //     input: "./src/index.js",
+    //     output: {
+    //         file: `dist/${pkg.name}.min.js`, // 导出文件
+    //         format: "umd", // 打包文件支持的形式
+    //         name: pkg.name,
+    //         exports: 'named',
+    //     },
+    //     plugins: [
+    //         commonjs(),
+    //         resolve(),
+    //         filesize(),
+    //         babel({
+    //             exclude: "node_modules/**",
+    //             babelHelpers: 'bundled',
+    //         }),
+    //         terser()
+    //     ],
+    // },
+    
+]
